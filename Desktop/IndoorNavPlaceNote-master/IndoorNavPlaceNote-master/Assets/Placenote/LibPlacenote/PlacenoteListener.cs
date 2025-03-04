07f20d13c6ce63a74ec0fef06864567703fa9fe3
tushar101-cloud <et21bthcs025@kazirangauniversity.in>
2025-03-04 21:46:37 +0530

    Delete Desktop directory

--- Desktop/IndoorNavPlaceNote-master/IndoorNavPlaceNote-master/Assets/Placenote/LibPlacenote/PlacenoteListener.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-public interface PlacenoteListener
-{
-	void OnPose(Matrix4x4 outputPose, Matrix4x4 arkitPose);
-	void OnStatusChange(LibPlacenote.MappingStatus prevStatus, LibPlacenote.MappingStatus currStatus);
-}
-
